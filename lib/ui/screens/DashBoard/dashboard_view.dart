3ae724b7fdfa9c16062b456c16b639f48246d55e
sameemakallayil <sameemakallayil@gmail.com>
2024-07-08 20:29:19 +0530

    update

--- lib/ui/screens/DashBoard/dashboard_view.dart
+++ lib/ui/screens/DashBoard/dashboard_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
 
 import 'dashboard_viewmodel.dart';
@@ -19,51 +20,61 @@ class DashboardView extends StatelessWidget {
           children: viewModel.screens,
           onPageChanged: viewModel.onChanged,
           physics: NeverScrollableScrollPhysics(),
+
+        ),
+        bottomNavigationBar: Consumer<DashboardViewModel>(
+          builder: (context, userModel, child) {
+           return NavigationBar(
+             onDestinationSelected:
+             viewModel.Destinationselected,
+
+
+
+             indicatorColor: Color(0xff998558),
+
+             indicatorShape:CircleBorder(),
+             selectedIndex: viewModel.currentPageIndex,
+             backgroundColor:Colors.white,
+             destinations: <NavigationDestination>[
+               NavigationDestination(
+                 icon: Icon(Icons.home_outlined,),
+                 selectedIcon: Icon(Icons.home,
+                   color: viewModel.currentPageIndex==0?Colors.white:null,),
+                 label: '',
+                 enabled: true,
+               ),
+               NavigationDestination(
+                 icon: Icon(Icons.dashboard_customize_outlined),
+                 selectedIcon: Icon(Icons.dashboard_customize_outlined,
+                   color: viewModel.currentPageIndex==1?Colors.white:null,),
+                 label: '',
+               ),
+               NavigationDestination(
+                 icon: Icon(Icons.favorite_border),
+                 selectedIcon: Icon(Icons.favorite_border,
+                   color: viewModel.currentPageIndex==2?Colors.white:null,),
+                 label: '',
+               ),
+               NavigationDestination(
+                 icon: Icon(Icons.notifications_none),
+                 selectedIcon: Icon(Icons.notifications_none,
+                   color: viewModel.currentPageIndex==3?Colors.white:null,),
+
+                 label: '',
+               ),
+               NavigationDestination(
+                 icon: Icon(Icons.account_circle_outlined),
+                 selectedIcon: Icon(Icons.account_circle_outlined,
+                   color: viewModel.currentPageIndex==4?Colors.white:null,),
+
+                 label: '',
+               ),
+             ],
+           );
+          },
         ),
 
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected:
-            viewModel.Destinationselected,
 
-          indicatorColor: Color(0xff998558),
-          indicatorShape:CircleBorder(),
-          selectedIndex: viewModel.selectedIndex,
-          backgroundColor:Colors.white,
-          destinations: <Widget>[
-            NavigationDestination(
-              icon: Icon(Icons.home_outlined,),
-              selectedIcon: Icon(Icons.home,
-                color: viewModel.selectedIndex==0?Colors.white:null,),
-              label: '',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.dashboard_customize_outlined),
-              selectedIcon: Icon(Icons.dashboard_customize_outlined,
-                color: viewModel.selectedIndex==1?Colors.white:null,),
-              label: '',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.favorite_border),
-              selectedIcon: Icon(Icons.favorite_border,
-                color: viewModel.selectedIndex==2?Colors.white:null,),
-              label: '',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.notifications_none),
-              selectedIcon: Icon(Icons.notifications_none,
-                color: viewModel.selectedIndex==3?Colors.white:null,),
-
-              label: '',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.account_circle_outlined),
-              selectedIcon: Icon(Icons.account_circle_outlined,
-                color: viewModel.selectedIndex==4?Colors.white:null,),
-
-              label: '',
-            ),
-          ],
-        ),
       ),
       viewModelBuilder: () => DashboardViewModel(),
     );
--- lib/ui/screens/DashBoard/dashboard_viewmodel.dart
+++ lib/ui/screens/DashBoard/dashboard_viewmodel.dart
@@ -11,8 +11,7 @@ import '../../../app/utils.dart';
 
 class DashboardViewModel extends BaseViewModel {
   int currentPageIndex = 0;
-  int selectedIndex=0;
- PageController pageController=PageController();
+ PageController pageController=PageController(initialPage: 0);
  List<Widget> screens=[
    HomePageView(),
    CategoriesView(),
@@ -20,24 +19,43 @@ class DashboardViewModel extends BaseViewModel {
    NotificationView(),
    ProfileView(),
  ];
- void onChanged(int index){
+  // int get selectedIndex => _selectedIndex;
+  // set selectedIndex(int index) {
+  //   _selectedIndex = index;
+  //   pageController.jumpToPage(index);
+  //   notifyListeners(); // Notify listeners of state change
+  // }
+  void onChanged(int index) {
+    currentPageIndex = index;
+    notifyListeners();// Update selected index
+  }
+  // void Destinationselected(int index) {
+  //   // print('Selected index changed to: $index');
+  //
+  //   currentPageIndex = index; // Update selected index
+  //   notifyListeners(); // Notify listeners of state change
+  //
+  // }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+    notifyListeners(); // Notify listeners of state change
 
-   selectedIndex=index;
-   notifyListeners();
-
- }
-
-  void Destinationselected(int selectedIndex) {
+  }
+  // Future<void> login(BuildContext context) async{}
+  void Destinationselected(int index)  {
     // currentPageIndex = index;
-    pageController.jumpToPage(selectedIndex);
+    currentPageIndex=index;
+    pageController.jumpToPage(index);
     notifyListeners();
   }
 
-  void updateTitle() {
-    notifyListeners();
-  }
+  // void updateTitle() {
+  //   notifyListeners();
+  // }
 
-  void nextPage() {
-    navigationService.navigateTo(Routes.notificationView);
-  }
+  // void nextPage() {
+  //   navigationService.navigateTo(Routes.notificationView);
+  // }
 }
